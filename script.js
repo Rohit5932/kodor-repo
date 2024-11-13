3b6d8caa1d530ef7b7d535725b04d1d1413bf124
charanbuchannola <buchannolacharan@gmail.com>
2024-11-13 15:06:20 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+function updateCounter() {
+  document.getElementById("counterValue").innerText = count;
+}
+
+function increase() {
+  count++;
+  updateCounter();
+}
+
+function decrease() {
+  count--;
+  updateCounter();
+}
+
+function reset() {
+  count = 0;
+  updateCounter();
+}
